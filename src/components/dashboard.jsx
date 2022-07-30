59632f383ff481e5a585d9b1ef1c890b24cce2bb
MaldingMalta <nischal.sha17@gmail.com>
2022-07-30 20:27:11 +0545

    'BASE'

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import IconButton from "@mui/material/IconButton";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import { sizing } from "@mui/system";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 580,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search"
+            inputProps={{ "aria-label": "Search" }}
+          />
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+            <SearchOutlinedIcon />
+          </IconButton>
+        </Paper>
+        <IconButton size="large">
+          <FilterAltOutlinedIcon />
+        </IconButton>
+        <IconButton size="large">
+          <AddCircleOutlineOutlinedIcon />
+        </IconButton>
+      </div>
+      <h2>Number of Reports:3</h2>
+      <Paper elevation={3} variant="outlined" sx={{ height: 100, gap: 2 }}>
+        Hello
+      </Paper>
+      <Paper elevation={3} variant="outlined" sx={{ height: 100 }}>
+        Hello
+      </Paper>
+      <Paper elevation={3} variant="outlined" sx={{ height: 100 }}>
+        Hello
+      </Paper>
+    </>
+  );
+};
+
+export default Dashboard;
